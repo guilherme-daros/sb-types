dca8d0113372b52ce43d3c0963fd41839770971d
guilherme-daros <gh5777.gh@gmail.com>
2026-02-25 12:00:41 -0300

    Add .gitignore and remove build artifacts

--- build/examples/SingletonApp/CMakeFiles/SingletonApp.dir/Main.cpp.o.ddi.d
+++
@@ -1,4 +0,0 @@
-examples/SingletonApp/CMakeFiles/SingletonApp.dir/Main.cpp.o.ddi: \
- /home/geagaa/workspace/playground/antimony-types/examples/SingletonApp/Main.cpp \
- /usr/include/stdc-predef.h \
- /home/geagaa/workspace/playground/antimony-types/include/types/Singleton.hpp
